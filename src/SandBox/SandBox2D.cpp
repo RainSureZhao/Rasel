4a9b540d0b03294a713ae0db366f1519ceb909ab
RainSure <1114560341@qq.com>
2023-09-15 10:23:32 +0800

    添加了渲染2D物体时Draw等数据的记录功能

--- src/SandBox/SandBox2D.cpp
+++ src/SandBox/SandBox2D.cpp
@@ -23,13 +23,13 @@ void SandBox2D::OnDetach() {
 
 void SandBox2D::OnUpdate(Rasel::Timestep ts) {
 
-
     RZ_PROFILE_FUNCTION();
 
     // Update
     m_CameraController.OnUpdate(ts);
     
     //Render
+    Rasel::Renderer2D::ResetStats();
     {
         RZ_PROFILE_SCOPE("Renderer Prep");
         Rasel::RendererCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
@@ -43,8 +43,17 @@ void SandBox2D::OnUpdate(Rasel::Timestep ts) {
         Rasel::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
         Rasel::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
         Rasel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-        Rasel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+        Rasel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
         Rasel::Renderer2D::DrawRotatedQuad({-2.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, rotation, m_CheckerboardTexture, 20.0f);
+        Rasel::Renderer2D::EndScene();
+        
+        Rasel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+        for(float y = -5.0f; y < 5.0f; y += 0.5f) {
+            for(float x = -5.0f; x < 5.0f; x += 0.5f) {
+                glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.5f };
+                Rasel::Renderer2D::DrawQuad({x, y}, {0.45f, 0.45f}, color);
+            }
+        }
         Rasel::Renderer2D::EndScene();
     }
 }
--- src/engine/Renderer/Renderer2D.cpp
+++ src/engine/Renderer/Renderer2D.cpp
@@ -21,9 +21,9 @@ namespace Rasel {
     };
     
     struct Renderer2DData {
-        const uint32_t MaxQuads = 10000;
-        const uint32_t MaxVertices = MaxQuads * 4;
-        const uint32_t MaxIndices = MaxQuads * 6;
+        static const uint32_t MaxQuads = 20000;
+        static const uint32_t MaxVertices = MaxQuads * 4;
+        static const uint32_t MaxIndices = MaxQuads * 6;
         static const uint32_t MaxTextureSlots = 32;
         
         Ref<VertexArray> QuadVertexArray;
@@ -38,7 +38,9 @@ namespace Rasel {
         std::array<Ref<Texture2D>, MaxTextureSlots> TextureSlots;
         uint32_t TextureSlotIndex = 1; // 0 = white texture
         
-        glm::vec4 QuadVertexPositions[4];
+        glm::vec4 QuadVertexPositions[4] = {};
+        
+        Renderer2D::Statistics Stats;
     };
     
     static Renderer2DData s_Data;
@@ -131,6 +133,10 @@ namespace Rasel {
     void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const glm::vec4 &color) {
         RZ_PROFILE_FUNCTION();
         
+        if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices) {
+            FlushAndReset();
+        }
+        
         const float textureIndex = 0.0f; // White Texture
         const float tilingFactor = 1.0f;
         
@@ -166,6 +172,8 @@ namespace Rasel {
         s_Data.QuadVertexBufferPtr ++;
         
         s_Data.QuadIndexCount += 6;
+        
+        s_Data.Stats.QuadCount ++;
     }
     
 
@@ -178,6 +186,11 @@ namespace Rasel {
     void Renderer2D::DrawQuad(const glm::vec3 &position, const glm::vec2 &size, const Ref<Texture2D> &texture,
                               float tilingFactor, const glm::vec4 &tintColor) {
         RZ_PROFILE_FUNCTION();
+        
+        if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices) {
+            FlushAndReset();
+        }
+        
         constexpr glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
         
         float textureIndex = 0.0f;
@@ -226,6 +239,8 @@ namespace Rasel {
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+        
+        s_Data.Stats.QuadCount ++;
 
 #if OLD_PATH
         s_Data.TextureShader->SetFloat4("u_Color", tintColor);
@@ -249,6 +264,10 @@ namespace Rasel {
     void Renderer2D::DrawRotatedQuad(const glm::vec3 &position, const glm::vec2 &size, float rotation,
                                      const glm::vec4 &color) {
         RZ_PROFILE_FUNCTION();
+        
+        if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices) {
+            FlushAndReset();
+        }
         
         const float textureIndex = 0.0f; // White Texture
         const float tilingFactor = 1.0f;
@@ -286,6 +305,8 @@ namespace Rasel {
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+        
+        s_Data.Stats.QuadCount ++;
     }
 
     void Renderer2D::DrawRotatedQuad(const glm::vec2 &position, const glm::vec2 &size, float rotation,
@@ -296,6 +317,10 @@ namespace Rasel {
     void Renderer2D::DrawRotatedQuad(const glm::vec3 &position, const glm::vec2 &size, float rotation,
                                      const Ref<Texture2D> &texture, float tilingFactor, const glm::vec4 &tintColor) {
         RZ_PROFILE_FUNCTION();
+        
+        if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices) {
+            FlushAndReset();
+        }
         
         constexpr glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
         
@@ -346,6 +371,8 @@ namespace Rasel {
         s_Data.QuadVertexBufferPtr++;
 
         s_Data.QuadIndexCount += 6;
+        
+        s_Data.Stats.QuadCount ++;
     }
 
     void Renderer2D::Flush() {
@@ -355,5 +382,23 @@ namespace Rasel {
         }
         
         RendererCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+        s_Data.Stats.DrawCalls ++;
+    }
+
+    void Renderer2D::FlushAndReset() {
+        EndScene();
+        
+        s_Data.QuadIndexCount = 0;
+        s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+        
+        s_Data.TextureSlotIndex = 1;
+    }
+
+    void Renderer2D::ResetStats() {
+        memset(&s_Data.Stats, 0, sizeof(Statistics));
+    }
+
+    Renderer2D::Statistics Renderer2D::GetStats() {
+        return s_Data.Stats;
     }
 } // Rasel
